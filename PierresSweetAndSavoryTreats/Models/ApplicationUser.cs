7eaf7e876590f0a41a77bf909e534f06b99eda2f
simioyin222 <simi.oyinkolade@gmail.com>
2024-02-16 13:46:10 -0600

    props for authentication and ensuring db context read

---
+++ PierresSweetAndSavoryTreats/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace YourProjectName.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        // Add additional properties here
+    }
+}
--- PierresSweetAndSavoryTreats/Program.cs
+++ PierresSweetAndSavoryTreats/Program.cs
@@ -1,18 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using PierresSweetAndSavoryTreats.Models;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
-
-
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-// Add DbContext with MySQL
+// Add DbContext with MySQL and Identity
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), 
-    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection")))); 
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))));
+
+// Add Identity services
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
@@ -28,6 +31,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication(); // Add authentication middleware
 app.UseAuthorization();
 
 app.MapControllerRoute(
