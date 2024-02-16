e6eb4be13286d256344ccc5ff9e64c198b19c772
simioyin222 <simi.oyinkolade@gmail.com>
2024-02-16 15:10:10 -0600

    add and fix db context to better align with models

--- PierresSweetAndSavoryTreats/Models/ApplicationDbContext.cs
+++ PierresSweetAndSavoryTreats/Models/ApplicationDbContext.cs
@@ -5,12 +5,30 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace PierresSweetAndSavoryTreats.Models
 {
-  public class ApplicationDbContext : IdentityDbContext<IdentityUser>
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
-        }
+  public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+  {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
-    // Define DbSets entities, e.g., public DbSet<Recipe> Recipes { get; set; }
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<TreatFlavor> TreatFlavors { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      base.OnModelCreating(builder);
+
+      builder.Entity<TreatFlavor>()
+        .HasKey(tf => new { tf.TreatId, tf.FlavorId });
+
+      builder.Entity<TreatFlavor>()
+        .HasOne(tf => tf.Treat)
+        .WithMany(t => t.Flavors)
+        .HasForeignKey(tf => tf.TreatId);
+
+      builder.Entity<TreatFlavor>()
+        .HasOne(tf => tf.Flavor)
+        .WithMany(f => f.Treats)
+        .HasForeignKey(tf => tf.FlavorId);
+    }
   }
 }
