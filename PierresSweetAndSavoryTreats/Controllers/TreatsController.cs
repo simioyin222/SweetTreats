97bbe29a8bec49582d51c7a6ea566ca1b5e1f512
simioyin222 <simi.oyinkolade@gmail.com>
2024-02-16 16:22:21 -0600

    add create and edit for treats

--- PierresSweetAndSavoryTreats/Controllers/TreatsController.cs
+++ PierresSweetAndSavoryTreats/Controllers/TreatsController.cs
@@ -21,5 +21,74 @@ namespace PierresSweetAndSavoryTreats.Controllers
         {
             return View(await _db.Treats.ToListAsync());
         }
+
+        // GET: Treats/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Treats/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TreatId,Name")] Treat treat)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(treat);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(treat);
+        }
+
+        // GET: Treats/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var treat = await _db.Treats.FindAsync(id);
+            if (treat == null)
+            {
+                return NotFound();
+            }
+            return View(treat);
+        }
+
+        // POST: Treats/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TreatId,Name")] Treat treat)
+        {
+            if (id != treat.TreatId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _db.Update(treat);
+                    await _db.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TreatExists(treat.TreatId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(treat);
+        }
     }
 }
