b7d61ba0d16f97cab5a82d17d32c7104b4fe75d7
simioyin222 <simi.oyinkolade@gmail.com>
2024-03-03 09:49:07 -0600

    add on

--- PierresSweetAndSavoryTreats/Controllers/FlavorsController.cs
+++ PierresSweetAndSavoryTreats/Controllers/FlavorsController.cs
@@ -2,11 +2,11 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using Sweets.Models;
+using PierresSweetAndSavoryTreats.Models;
 using System.Collections.Generic;
 using System.Linq; 
 
-namespace Sweets.Controllers
+namespace PierresSweetAndSavoryTreats.Controllers
 {
   [Authorize]
   public class FlavorsController : Controller
